db3b3a2d94e7b52d710da8be6283baace1a5aacb
Iñaki Mallol <javimallol@hotmail.com>
2021-09-13 19:59:54 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+*DICE GAME*
+
+It's a multiplayer dice game for 2 players
+
+Each player has its turn. The shadow below the numbers indicates whose turn it is. 
+When a player rolls the dice the app throws a random number between 1 and 6 and add it to the score of that player.
+
+If the player want to make a riskier move he can choose for "Double or Nothing". The app will randomly throw 0 or double the number that the dice gives. 
+
+The first player to get 21 points is the WINNER! 
+
+LET'S PLAY!
---
+++ index.js
@@ -0,0 +1,139 @@
+// Create variables for the game state
+let player1Score = 0
+let player2Score = 0
+let player1Turn = true
+
+// Create variables to store references to the necessary DOM nodes
+const player1Dice = document.getElementById("player1Dice")
+const player2Dice = document.getElementById("player2Dice")
+const player1Scoreboard = document.getElementById("player1Scoreboard")
+const player2Scoreboard = document.getElementById("player2Scoreboard")
+const message = document.getElementById("message")
+const rollBtn = document.getElementById("rollBtn")
+const resetBtn = document.getElementById("resetBtn")
+
+function showResetButton() {
+    rollBtn.style.display = "none"
+    resetBtn.style.display = "block"
+}
+
+let countBtn = 0;
+
+/* Hook up a click event listener to the Roll Dice Button. */
+ rollBtn.addEventListener("click", function() {
+    const randomNumber = Math.floor(Math.random() * 6) + 1
+    countBtn += 1
+    
+
+    if (player1Turn) {
+        player1Score += randomNumber
+        player1Scoreboard.textContent = player1Score
+        player1Dice.textContent = randomNumber
+        player1Dice.classList.remove("active")
+        player2Dice.classList.add("active")
+        message.textContent = 'Player 2 Turn'
+    } else {
+        player2Score += randomNumber
+        player2Scoreboard.textContent = player2Score
+        player2Dice.textContent = randomNumber
+        player2Dice.classList.remove("active")
+        player1Dice.classList.add("active")
+        message.textContent = "Player 1 Turn"
+    }
+    
+    if (player1Score >= 20 && countBtn % 2 === 0) {
+        message.textContent = "Player 1 Won 🥳"
+        showResetButton()
+    }  else if (player2Score >= 20 && countBtn % 2 === 0) {
+        message.textContent = "Player 2 Won 🎉"
+        showResetButton()
+    }
+    player1Turn = !player1Turn
+})
+ 
+
+const doubleBtn = document.getElementById('double')
+
+doubleBtn.addEventListener('click', function() {
+    const randomDouble = Math.random()
+    const randomNumber = Math.floor(Math.random() * 6) + 1
+    countBtn += 1
+
+    if (player1Turn === true && randomDouble > 0.5) {
+        player1Score += randomNumber*2
+        player1Scoreboard.textContent = player1Score
+        player1Dice.textContent = '🏆!'
+        player1Dice.classList.remove("active")
+        player2Dice.classList.add("active")
+    } else if (player1Turn === true && randomDouble < 0.5) {
+        player1Score = 0;
+        player1Scoreboard.textContent = player1Score
+        player1Dice.textContent = '😪'
+        player1Dice.classList.remove("active")
+        player2Dice.classList.add("active")
+    } else if (player1Turn === false && randomDouble > 0.5) {
+        player2Score += randomNumber *2 
+        player2Scoreboard.textContent = player2Score
+        player2Dice.textContent = '🏆!'
+        player2Dice.classList.remove("active")
+        player1Dice.classList.add("active")   
+    } else if (player1Turn === false && randomDouble < 0.5) {
+        player2Score = 0;
+        player2Scoreboard.textContent = player2Score
+        player2Dice.textContent = '😪'
+        player2Dice.classList.remove("active")
+        player1Dice.classList.add("active")
+    }
+
+    if (player1Score >= 20 && countBtn % 2 === 0) {
+        message.textContent = "Player 1 Won 🥳"
+        showResetButton()
+    }  else if (player2Score >= 20 && countBtn % 2 === 0) {
+        message.textContent = "Player 2 Won 🎉"
+        showResetButton()
+    }
+
+    player1Turn = !player1Turn
+})
+
+resetBtn.addEventListener("click", function(){
+    reset()
+})
+
+function reset() {
+    player1Score = 0
+    player2Score = 0
+    player1Turn = true
+    player1Scoreboard.textContent = 0
+    player2Scoreboard.textContent = 0
+    player1Dice.textContent = "-"
+    player2Dice.textContent = "-"
+    message.textContent = "Player 1 Turn"
+    resetBtn.style.display = "none"
+    rollBtn.style.display = "block"
+    player2Dice.classList.remove("active")
+    player1Dice.classList.add("active")
+}
+
+
+
+
+const setupDiv = document.getElementById("setup");
+const punchlineDiv = document.getElementById("punchline");
+const punchlineBtn = document.getElementById("punchlineBtn");
+const newJokeBtn = document.getElementById("newJokeBtn");
+let punchline;
+
+punchlineBtn.addEventListener("click", getPunchline)
+
+function getPunchline() {
+    punchlineDiv.innerHTML = punchline
+    punchlineDiv.classList.add('bubble')
+    punchlineBtn.classList.toggle('hidden')
+    newJokeBtn.classList.toggle('hidden')
+}
+
+newJokeBtn.addEventListener('click', getJoke)
+
+punchlineDiv.innerHTML = ""
+punchlineDiv.classList.remove('bubble')
